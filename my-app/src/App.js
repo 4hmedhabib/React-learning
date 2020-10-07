ca2bd8aa3fdf624336194fe76b4db9e1963c8c82
Ahmed Habib <axmedxabiib2018@gmail.com>
2020-10-07 19:22:36 +0300

    functional component Insert

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
+import Person from "./Person/Person";
 import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <h1>Hi, I am React 16.13 Version</h1>
+      <Person />
     </div>
   );
-}
+};
 
 export default App;
---
+++ my-app/src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const person = () => {
+  return <p> Hi I'm person </p>;
+};
+
+export default person;
