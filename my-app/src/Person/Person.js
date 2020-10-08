c425a9096cb8bd1bd2f9de0b579bc82cb5006329
Ahmed Habib <axmedxabiib2018@gmail.com>
2020-10-08 08:58:30 +0300

    adding css files

--- my-app/src/Person/Person.js
+++ my-app/src/Person/Person.js
@@ -1,12 +1,13 @@
 import React from "react";
+import "./Person.css";
 
 const person = (props) => {
   return (
-    <div>
+    <div className="Person">
       <p>
         Hi I'm {props.name} and I'am {props.age} years old.
       </p>
-      <input type="text" onChange={props.nameChange}></input>
+      <input type="text" onChange={props.nameChange} value={props.name}></input>
     </div>
   );
 };
