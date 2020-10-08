480d2b70c4a99bf5aeb526539faf5bd9557c7bb4
Ahmed Habib <axmedxabiib2018@gmail.com>
2020-10-08 09:40:43 +0300

    adding toggle handler

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -10,17 +10,18 @@ const App = (props) => {
       { name: "Marwa Habib", age: 12 },
     ],
     otherState: "otherState",
+    showPerson: false,
   });
 
-  const switchNameHandler = () => {
-    setPersonState({
-      persons: [
-        { name: "Ahmed E. ", age: 19 },
-        { name: "Nasri ", age: 14 },
-        { name: "Marwa ", age: 12 },
-      ],
-    });
-  };
+  // const switchNameHandler = () => {
+  //   setPersonState({
+  //     persons: [
+  //       { name: "Ahmed E. Habib", age: 19 },
+  //       { name: "Nasri Habib", age: 14 },
+  //       { name: "Marwa Habib", age: 12 },
+  //     ],
+  //   });
+  // };
 
   const nameChangeHandler = (event) => {
     setPersonState({
@@ -29,28 +30,42 @@ const App = (props) => {
         { name: event.target.value, age: 14 },
         { name: "Marwa Habib", age: 12 },
       ],
+      showPerson: personState.showPerson,
     });
+  };
+
+  const togglePersonHandler = () => {
+    const doesShow = personState.showPerson;
+    const persons = personState.persons;
+    setPersonState({ persons: persons, showPerson: !doesShow });
   };
 
   return (
     <div className="App">
-      <button onClick={switchNameHandler} className="btnHandler">
-        Toggle
+      <button
+        className="btnHandler"
+        onClick={togglePersonHandler /*switchNameHandler*/}
+      >
+        Toggle Persons
       </button>
-      <h1>Hi, I am React 16.13 Version</h1>
-      <Person
-        name={personState.persons[0].name}
-        age={personState.persons[0].age}
-      />
-      <Person
-        name={personState.persons[1].name}
-        age={personState.persons[1].age}
-        nameChange={nameChangeHandler}
-      />
-      <Person
-        name={personState.persons[2].name}
-        age={personState.persons[2].age}
-      />
+
+      {personState.showPerson ? (
+        <div>
+          <Person
+            name={personState.persons[0].name}
+            age={personState.persons[0].age}
+          />
+          <Person
+            name={personState.persons[1].name}
+            age={personState.persons[1].age}
+            nameChange={nameChangeHandler}
+          />
+          <Person
+            name={personState.persons[2].name}
+            age={personState.persons[2].age}
+          />
+        </div>
+      ) : null}
     </div>
   );
 };
