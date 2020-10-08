6a0e53a94ff6b3c906f937b3b236596758ef3946
Ahmed Habib <axmedxabiib2018@gmail.com>
2020-10-08 06:30:28 +0300

    Using State

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,15 +3,22 @@ import Person from "./Person/Person";
 import "./App.css";
 
 const App = () => {
+  const state = {
+    persons: [
+      { name: "Ahmed E. Habib", age: 19 },
+      { name: "Nasri Habib", age: 14 },
+      { name: "Marwa Habib", age: 12 },
+    ],
+  };
+
   return (
     <div className="App">
       <h1>Hi, I am React 16.13 Version</h1>
-      <Person name="Ahmed Habib" age="19" />
-      <Person name="Abdi Habib" age="16">
-        {" "}
-        ,My Hoppies: Racing
+      <Person name={state.persons[0].name} age={state.persons[0].age} />
+      <Person name={state.persons[1].name} age={state.persons[1].age}>
+        My Hoppies: Racing
       </Person>
-      <Person name="Marwa Habib" age="12" />
+      <Person name={state.persons[2].name} age={state.persons[2].age} />
     </div>
   );
 };
