e455e47f5463867cc8a40d57a0509adf8df12d9d
Ahmed Habib <axmedxabiib2018@gmail.com>
2020-10-08 08:30:49 +0300

    Handler lesson

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,29 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import Person from "./Person/Person";
 import "./App.css";
 
-const App = () => {
-  const state = {
+const App = (props) => {
+  const [personState, setPersonState] = useState({
     persons: [
       { name: "Ahmed E. Habib", age: 19 },
       { name: "Nasri Habib", age: 14 },
       { name: "Marwa Habib", age: 12 },
     ],
-  };
+    otherState: "otherState",
+  });
 
   const switchNameHandler = () => {
-    console.log("Was Clicked");
+    setPersonState({
+      persons: [
+        { name: "Ahmed E. ", age: 19 },
+        { name: "Nasri ", age: 14 },
+        { name: "Marwa ", age: 12 },
+      ],
+    });
   };
 
   return (
     <div className="App">
       <button onClick={switchNameHandler}>Switch Button</button>
       <h1>Hi, I am React 16.13 Version</h1>
-      <Person name={state.persons[0].name} age={state.persons[0].age} />
-      <Person name={state.persons[1].name} age={state.persons[1].age}>
-        My Hoppies: Racing
-      </Person>
-      <Person name={state.persons[2].name} age={state.persons[2].age} />
+      <Person
+        name={personState.persons[0].name}
+        age={personState.persons[0].age}
+      />
+      <Person
+        name={personState.persons[1].name}
+        age={personState.persons[1].age}
+      />
+      <Person
+        name={personState.persons[2].name}
+        age={personState.persons[2].age}
+      />
     </div>
   );
 };
